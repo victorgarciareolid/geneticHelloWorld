ec7975e9d14d01440a51fbca93be5bbaa8f157a5
Victor Garcia <victorgarciavilu@gmail.com>
2018-05-31 00:34:35 +0200

    :100:

--- geneticAlgorithm.py
+++ geneticAlgorithm.py
@@ -56,17 +56,14 @@ class Chromosome:
         child2_word_list = list(partner.word)
 
         for i in range(crossover):
-            if np.random.random_sample() < mutation_probability:
+            if np.random.random_sample() < mutation_probability :
                 # Mutation
                 child1_word_list[i] = random_char()
-            else:
-                child1_word_list[i] = list(partner.word)[i]
-
-            if np.random.random_sample() < mutation_probability:
-                # Mutation
                 child2_word_list[i] = random_char()
             else:
+                child1_word_list[i] = list(partner.word)[i]
                 child1_word_list[i] = list(self.word)[i]
+
 
         child1.setWord("".join(child1_word_list))
         child2.setWord("".join(child2_word_list))
@@ -117,13 +114,13 @@ class Population:
 
 
 def main():
-    p = Population(5, 4, "hola", 4)
+    p = Population(5, 4, "hola", 5)
 
     print(p)
 
     s = ""
     while s != "hola":
-        p.evolve(3, 0.5)
+        p.evolve(4, 0.25)
         s = p.individuals[0].word
         print(s)
 
